1630d7668224a7a1d0af38ced48b93cd1d45eaf1
CamilleAbella <camille.abella@hotmail.fr>
2021-03-11 09:53:30 +0100

    add sass task and sass watcher

--- dist/bundle.js
+++ dist/bundle.js
@@ -11,8 +11,10 @@ var app = (() => {
     hello: () => hello
   });
   function hello() {
-    return "Hello World!";
+    const h1 = document.createElement("h1");
+    h1.innerHTML = "Hello World!";
+    document.body.appendChild(h1);
   }
   return src_exports;
 })();
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsic3JjL2luZGV4LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJleHBvcnQgZnVuY3Rpb24gaGVsbG8oKXtcclxuICByZXR1cm4gXCJIZWxsbyBXb3JsZCFcIjtcclxufSJdLAogICJtYXBwaW5ncyI6ICI7Ozs7Ozs7O0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBTztBQUNMLFdBQU87QUFBQTsiLAogICJuYW1lcyI6IFtdCn0K
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsic3JjL2luZGV4LnRzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJleHBvcnQgZnVuY3Rpb24gaGVsbG8oKSB7XG4gIGNvbnN0IGgxID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcImgxXCIpXG5cbiAgaDEuaW5uZXJIVE1MID0gXCJIZWxsbyBXb3JsZCFcIlxuXG4gIGRvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoaDEpXG59XG4iXSwKICAibWFwcGluZ3MiOiAiOzs7Ozs7OztBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQU87QUFDTCxVQUFNLEtBQUssU0FBUyxjQUFjO0FBRWxDLE9BQUcsWUFBWTtBQUVmLGFBQVMsS0FBSyxZQUFZO0FBQUE7IiwKICAibmFtZXMiOiBbXQp9Cg==
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export function hello() {
-  return "Hello World!"
+  const h1 = document.createElement("h1")
+
+  h1.innerHTML = "Hello World!"
+
+  document.body.appendChild(h1)
 }
